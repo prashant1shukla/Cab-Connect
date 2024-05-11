0ea56f947a9b444891618794db1b57b78a0d8296
prashant1shukla <prashantshukla@gmail.com>
2024-05-12 00:48:27 +0530

    Homework-3

---
+++ homework-3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+public class Logger : IDisposable
+{
+    private static Logger? instance; 
+    private readonly string logFilePath;
+    private StreamWriter? logWriter;
+
+    private Logger(string logFilePath)
+    {
+        this.logFilePath = logFilePath;
+        logWriter = new StreamWriter(logFilePath, true);
+    }
+
+    public static Logger Instance
+    {
+        get
+        {
+            instance ??= new Logger("log.txt");
+            return instance;
+        }
+    }
+
+    private void Log(string message)
+    {
+        logWriter?.WriteLine($"{DateTime.Now}: {message}");
+        logWriter?.Flush();
+    }
+
+    public void Dispose()
+    {
+        logWriter?.Dispose();
+    }
+
+    public static void Main(string[] args)
+    {
+        using var logger = Logger.Instance;
+        logger.Log("Logging a message to the log file.");
+    }
+}
