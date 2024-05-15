1dec0345f6588821b2c86690f1fbac789b0abf3d
Prashant Shukla <prashant.shukla@kickdrumtech.com>
2024-05-16 01:53:58 +0530

    Homework-7

---
+++ homework-7/homework-7/Controllers/RegsiterController.cs
@@ -0,0 +1,17 @@
+﻿using homework_7.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace homework_7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RegsiterController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register(UserRegistrationDTO userDTO)
+        {
+            return Ok("User registered successfully!");
+        }
+    }
+}
---
+++ homework-7/homework-7/Validators/CVVAttribute .cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace homework_7.Validators
+{
+    public class CVVAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || !(value is string cvv))
+                return ValidationResult.Success;
+
+            if (cvv.Length < 3 || cvv.Length > 4 || !int.TryParse(cvv, out _))
+            {
+                return new ValidationResult("CVV must be a 3 or 4-digit number.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+}
